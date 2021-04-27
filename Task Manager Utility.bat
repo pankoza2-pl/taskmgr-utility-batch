13f30708ea12a35abed56a45fc86d809e043b783
pankoza-pl <77279607+pankoza-pl@users.noreply.github.com>
2021-04-27 09:36:47 +0200

    add start task manager

--- Task Manager Utility.bat
+++ Task Manager Utility.bat
@@ -1,12 +1,14 @@
 @echo off
-title Task Manager Utility by Creper132 (Run as Administrator)
+title Task Manager Utility by Creper132 (modded by pankoza-pl) (Run as Administrator)
 echo (1) Turn on task manager
 echo (2) Turn off task manager
 echo (3) Kill task manager
+echo (4) Start Task Manager
 set /p select=Select: 
 if %select%==1 goto 1
 if %select%==2 goto 2
 if %select%==3 goto 3
+if %select%==4 goto 4
 :1
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
 exit
@@ -15,4 +17,7 @@ reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v Disabl
 exit
 :3
 taskkill /f /im taskmgr.exe /t
+exit
+:4
+start taskmgr.exe
 exit
